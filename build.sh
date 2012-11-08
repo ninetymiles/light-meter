a4824b8c78314475fa75935f9b5922c6ec03c5a2
Timon <timonlio@gmail.com>
2012-11-08 20:57:29 +0800

    FEATURE: Update build script, auto use version.name from local.properties DESC: None

--- build.sh
+++ build.sh
@@ -4,13 +4,17 @@ TARGET=$1
 PWD=$(pwd)
 PROJECT_NAME=$(cat build.xml | grep "project name" | awk -F"\"" '{print $2}')
 VERSION_NAME=$(cat AndroidManifest.xml | grep "versionName" | awk -F"\"" '{print $2}')
+VERSION_NAME=$(cat local.properties | grep "version.name" | awk -F"=" '{print $2}')
+VERSION_CODE=$(cat local.properties | grep "version.code" | awk -F"=" '{print $2}')
 
 echo "ProjectName:${PROJECT_NAME}"
 echo "VersionName:${VERSION_NAME}"
+echo "VersionCode:${VERSION_CODE}"
 echo ""
 
 if [ x"$TARGET" = "xrelease" -o x"$TARGET" = "xall" ]; then
-    ant -q clean
-    ant -q release
+    sed -i "/versionName/s/\".*\"/\"${VERSION_NAME}\"/" AndroidManifest.xml
+    ant clean release
+    git checkout AndroidManifest.xml
 fi
 
