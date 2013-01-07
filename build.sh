d0076d87dead777b75047541aa20bfe467a5aa03
Timon <timonlio@gmail.com>
2013-01-07 13:54:26 +0800

    FEATURE: Count commits for versionCode DESC: None

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ PROJECT_NAME=$(cat build.xml | grep "project name" | awk -F"\"" '{print $2}')
 VERSION_NAME=$(cat AndroidManifest.xml | grep "versionName" | awk -F"\"" '{print $2}')
 VERSION_NAME=$(cat local.properties | grep "version.name" | awk -F"=" '{print $2}')
 VERSION_CODE=$(cat local.properties | grep "version.code" | awk -F"=" '{print $2}')
+VERSION_CODE=$(git log | grep commit | wc -l)
 
 echo "ProjectName:${PROJECT_NAME}"
 echo "VersionName:${VERSION_NAME}"
@@ -17,6 +18,12 @@ if [ x"$TARGET" = "x" ]; then
     echo "    bulid.sh TARGET";
     echo "";
     exit 1;
+fi
+
+if [ -z "$(grep "version.code" local.properties)" ]; then
+    echo "version.code=$VERSION_CODE" >> local.properties
+else
+    sed -i "s/version\.code=.*/version\.code=$VERSION_CODE/" local.properties
 fi
 
 if [ x"$TARGET" = "xrelease" -o x"$TARGET" = "xall" ]; then
