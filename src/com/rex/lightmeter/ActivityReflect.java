b85d65b1a61cca25fbddf9fc939701921b5d5a6a
Timon <timonlio@gmail.com>
2013-10-29 21:49:52 +0800

    [FEATURE] Add lib-metadata-extractor to read exif info from camera preview

--- src/com/rex/lightmeter/ActivityReflect.java
+++ src/com/rex/lightmeter/ActivityReflect.java
@@ -1,11 +1,8 @@
 package com.rex.lightmeter;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import android.app.Activity;
@@ -13,12 +10,9 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
-import android.hardware.Camera.Parameters;
 import android.hardware.Camera.PictureCallback;
 import android.hardware.Camera.Size;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -32,6 +26,13 @@ import android.view.View.OnClickListener;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import com.drew.metadata.exif.ExifSubIFDDescriptor;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
 import com.rex.flurry.FlurryAgentWrapper;
 
 public class ActivityReflect extends Activity {
@@ -261,28 +262,43 @@ public class ActivityReflect extends Activity {
 	};
 	
 	private PictureCallback mPictureCallback = new PictureCallback() {
-		
-		public static final int MEDIA_TYPE_IMAGE = 1;
-		public static final int MEDIA_TYPE_VIDEO = 2;
-		
 		@Override
 		public void onPictureTaken(byte[] data, Camera camera) {
 			if (DEBUG) Log.v(TAG, "ActivityReflect::onPictureTaken");
-			File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
-			if (pictureFile == null) {
-				if (DEBUG) Log.d(TAG, "ActivityReflect::onPictureTaken Error creating media file, check storage permissions");
-				return;
-			}
 			
+			BufferedInputStream stream = new BufferedInputStream(new ByteArrayInputStream(data));
+			Metadata metadata = null;
 			try {
-				FileOutputStream fos = new FileOutputStream(pictureFile);
-				fos.write(data);
-				fos.close();
-			} catch (FileNotFoundException e) {
-				Log.w(TAG, "ActivityReflect::onPictureTaken File not found: " + e.toString());
-			} catch (IOException e) {
-				Log.w(TAG, "ActivityReflect::onPictureTaken Error accessing file: " + e.toString());
+				metadata = ImageMetadataReader.readMetadata(stream, false);
+			} catch (IOException ex) {
+				Log.e(TAG, "ActivityReflect::onPictureTaken read meta data failed", ex);
+			} catch (ImageProcessingException ex) {
+				Log.w(TAG, "ActivityReflect::onPictureTaken image process failed", ex);
 			}
+			for (Directory directory : metadata.getDirectories()) {
+				for (Tag tag : directory.getTags()) {
+					Log.i(TAG, tag.toString());
+				}
+			}
+			// obtain the Exif directory
+			ExifSubIFDDirectory directory = metadata.getDirectory(ExifSubIFDDirectory.class);
+			ExifSubIFDDescriptor descriptor = new ExifSubIFDDescriptor(directory);
+			
+			// obtain 
+			String aperture = directory.getString(ExifSubIFDDirectory.TAG_APERTURE);
+			String shutter = directory.getString(ExifSubIFDDirectory.TAG_SHUTTER_SPEED);
+			String iso = directory.getString(ExifSubIFDDirectory.TAG_ISO_EQUIVALENT);
+			Log.i(TAG, "Activityeflect::onPictureTaken aperture:" + aperture + " shutter:" + shutter + " iso:" + iso);
+			
+			String apertureDesc = descriptor.getApertureValueDescription();
+			String shutterDesc = descriptor.getShutterSpeedDescription();
+			String isoDesc = descriptor.getIsoEquivalentDescription();
+			String evDesc = descriptor.getExposureProgramDescription();
+			Log.i(TAG, "Activityeflect::onPictureTaken" +
+					" apertureDesc:" + apertureDesc + 
+					" shutterDesc:" + shutterDesc + 
+					" isoDesc:" + isoDesc +
+					" evDesc:" + evDesc);
 			
 			// Camera HAL of some devices have a bug. 
 			// Starting preview immediately after taking a picture will fail. 
@@ -294,57 +310,6 @@ public class ActivityReflect extends Activity {
 					setupPreview();
 				}
 			}, 300);
-			
-			// Calculate the width and the height of the jpeg.
-			Parameters param = camera.getParameters();
-			Size s = param.getPictureSize();
-			if (DEBUG) Log.v(TAG, "ActivityReflect::onPictureTaken width:" + s.width + " height:" + s.height);
-			
-//			int orientation = Exif.getOrientation(data);
-//			int width, height;
-//			if ((mJpegRotation + orientation) % 180 == 0) {
-//				width = s.width;
-//				height = s.height;
-//			} else {
-//				width = s.height;
-//				height = s.width;
-//			}
-		}
-		
-		/** Create a file Uri for saving an image or video */
-		private Uri getOutputMediaFileUri(int type) {
-			return Uri.fromFile(getOutputMediaFile(type));
-		}
-
-		/** Create a File for saving an image or video */
-		private File getOutputMediaFile(int type){
-			// To be safe, you should check that the SDCard is mounted
-			// using Environment.getExternalStorageState() before doing this.
-
-			File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyCameraApp");
-			// This location works best if you want the created images to be shared
-			// between applications and persist after your app has been uninstalled.
-
-			// Create the storage directory if it does not exist
-			if (!mediaStorageDir.exists()) {
-				if (!mediaStorageDir.mkdirs()) {
-					Log.d("MyCameraApp", "failed to create directory");
-					return null;
-				}
-			}
-
-			// Create a media file name
-			String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-			File mediaFile;
-			if (type == MEDIA_TYPE_IMAGE) {
-				mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
-			} else if (type == MEDIA_TYPE_VIDEO) {
-				mediaFile = new File(mediaStorageDir.getPath() + File.separator + "VID_" + timeStamp + ".mp4");
-			} else {
-				return null;
-			}
-
-			return mediaFile;
 		}
 	};
 }
