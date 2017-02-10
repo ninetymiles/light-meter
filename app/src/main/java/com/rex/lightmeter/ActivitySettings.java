3fd0cc3c3847fd7e2edbb1d31d32e61c6fef1f97
timonlio <timonlio@gmail.com>
2017-02-10 22:10:23 +0800

    Refine settings page, remove unused code

--- app/src/main/java/com/rex/lightmeter/ActivitySettings.java
+++ app/src/main/java/com/rex/lightmeter/ActivitySettings.java
@@ -17,23 +17,23 @@
 package com.rex.lightmeter;
 
 import android.app.ActionBar;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceManager;
 import android.view.MenuItem;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ActivitySettings extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class ActivitySettings extends PreferenceActivity {
 
     private final Logger mLogger = LoggerFactory.getLogger("RexLog");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mLogger.trace("");
+
         addPreferencesFromResource(R.xml.settings);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
@@ -42,8 +42,6 @@ public class ActivitySettings extends PreferenceActivity implements SharedPrefer
                 actionBar.setDisplayHomeAsUpEnabled(true);
             }
         }
-
-        PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
@@ -56,11 +54,6 @@ public class ActivitySettings extends PreferenceActivity implements SharedPrefer
     protected void onStop() {
         super.onStop();
         mLogger.trace("");
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        mLogger.trace("key:{}", key);
     }
 
     @Override
